708deb882094347bc77bca71f92cb1d846063310
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-11-27 04:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function setClock() {
 
   // Calculate the rotation for each hand  
   const secondsDegrees = ((seconds / 60) * 360) + 90; // 0 degrees is at 3 o'clock position  
-  const minutesDegrees = ((minutes / 60) * 360) + (seconds / 60) * 6 + 90; // include seconds for smooth transition  
-  const hoursDegrees = ((hours / 12) * 360) + (minutes / 60) * 30 + 90; // include minutes for smooth transition  
+	const minutesDegrees = ((minutes + seconds/60) / 60) * 360 + 90;
+	const hoursDegrees = ((hours / 12) * 360) + (minutes / 60) * 30 + 90; // include minutes for smooth transition  
 
   // Apply rotation to the hands  
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;  
